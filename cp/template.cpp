9d7cc131641197f2f8de5fa532059d392833e218
huynhanx03 <157712338+huynhanx03@users.noreply.github.com>
2026-01-30 09:46:49 +0700

    Add cp

---
+++ cp/template.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <climits>
+#include <numeric>
+#include <iomanip>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+#define nhan43                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+void fastIO()
+{
+    nhan43;
+    if (fopen("main.inp", "r"))
+    {
+        freopen("main.inp", "r", stdin);
+        freopen("main.out", "w", stdout);
+    }
+}
+
+class Solution
+{
+public:
+    void sol()
+    {
+        
+    };
+};
+
+int main()
+{
+    fastIO();
+    int tc = 1;
+    cin >> tc;
+    Solution sol;
+    while (tc--)
+    {
+        sol.sol();
+    }
+    return 0;
+}
