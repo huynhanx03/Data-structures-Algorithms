6669975b6b1766dce920e569474bf40f173a1338
huynhanx03 <157712338+huynhanx03@users.noreply.github.com>
2026-03-03 13:48:00 +0700

    Time: 0 ms (100.00%), Space: 21.9 MB (44.74%) - Validate Binary Search Tree

---
+++ leetcode/Medium/0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -0,0 +1,32 @@
+/*
+ Problem: 98. Validate Binary Search Tree
+ Language: cpp
+ Runtime: 0 ms (100.00%)
+ Memory: 21.9 MB (44.74%)
+ Tags: Tree, Depth-First Search, Binary Search Tree, Binary Tree
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return valid(root, LONG_MIN, LONG_MAX);        
+    }
+private:
+    bool valid(TreeNode* node, long minimum, long maximum) {
+        if (!node) return true;
+
+        if (!(node->val > minimum && node->val < maximum)) return false;
+
+        return valid(node->left, minimum, node->val) && valid(node->right, node->val, maximum);
+    }    
+};
