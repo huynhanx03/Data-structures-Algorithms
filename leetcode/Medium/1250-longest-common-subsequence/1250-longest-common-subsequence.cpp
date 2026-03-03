a9c20e95a3e73c6a6a1786d8c19cb0c836b020b2
huynhanx03 <157712338+huynhanx03@users.noreply.github.com>
2026-03-03 13:47:51 +0700

    Time: 27 ms (58.62%), Space: 27.3 MB (69.15%) - Longest Common Subsequence

---
+++ leetcode/Medium/1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -0,0 +1,26 @@
+/*
+ Problem: 1250. Longest Common Subsequence
+ Language: cpp
+ Runtime: 27 ms (58.62%)
+ Memory: 27.5 MB (53.27%)
+ Tags: String, Dynamic Programming
+*/
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size(), m = text2.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (text1[i - 1] == text2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        return dp[n][m];
+    }
+};
