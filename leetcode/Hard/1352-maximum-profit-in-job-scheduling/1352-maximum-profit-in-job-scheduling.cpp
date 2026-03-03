d7a9adef4f191b7fdcaed1c99623482fcf64e838
huynhanx03 <157712338+huynhanx03@users.noreply.github.com>
2026-03-03 13:47:48 +0700

    Time: 11 ms (99.84%), Space: 75.3 MB (98.03%) - Maximum Profit in Job Scheduling

---
+++ leetcode/Hard/1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.cpp
@@ -0,0 +1,25 @@
+/*
+ Problem: 1352. Maximum Profit in Job Scheduling
+ Language: cpp
+ Runtime: 87 ms (29.68%)
+ Memory: 98.8 MB (29.12%)
+ Tags: Array, Binary Search, Dynamic Programming, Sorting
+*/
+class Solution {
+public:
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        int n = startTime.size();
+        vector<vector<int>> jobs;
+        for (int i = 0; i < n; ++i) {
+            jobs.push_back({endTime[i], startTime[i], profit[i]});
+        }
+        sort(jobs.begin(), jobs.end());
+        map<int, int> dp = {{0, 0}};
+        for (auto& job : jobs) {
+            int cur = prev(dp.upper_bound(job[1]))->second + job[2];
+            if (cur > dp.rbegin()->second)
+                dp[job[0]] = cur;
+        }
+        return dp.rbegin()->second;
+    }
+};
