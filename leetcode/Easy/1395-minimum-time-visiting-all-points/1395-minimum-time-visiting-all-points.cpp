2c38659f94e311e47f51be3988b6fce41170f8d0
huynhanx03 <157712338+huynhanx03@users.noreply.github.com>
2026-03-03 13:47:21 +0700

    Time: 0 ms (100.00%), Space: 13.7 MB (61.54%) - Minimum Time Visiting All Points

---
+++ leetcode/Easy/1395-minimum-time-visiting-all-points/1395-minimum-time-visiting-all-points.cpp
@@ -0,0 +1,21 @@
+/*
+ Problem: 1395. Minimum Time Visiting All Points
+ Language: cpp
+ Runtime: 0 ms (100.00%)
+ Memory: 13.7 MB (61.54%)
+ Tags: Array, Math, Geometry
+*/
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+         int ans = 0;
+
+        for (int i = 1; i < points.size(); i++) {
+            int dx = abs(points[i][0] - points[i - 1][0]);
+            int dy = abs(points[i][1] - points[i - 1][1]);
+            ans += max(dx, dy);
+        }
+
+        return ans;
+    }
+};
