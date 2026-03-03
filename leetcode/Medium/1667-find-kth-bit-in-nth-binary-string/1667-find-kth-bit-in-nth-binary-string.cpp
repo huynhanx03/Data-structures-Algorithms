b2e86dcce09e749525d2f4c0fa9ec0a6ab209f2c
huynhanx03 <157712338+huynhanx03@users.noreply.github.com>
2026-03-03 13:47:16 +0700

    Time: 0 ms (100.00%), Space: 7.8 MB (63.07%) - Find Kth Bit in Nth Binary String

---
+++ leetcode/Medium/1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
@@ -0,0 +1,23 @@
+/*
+ Problem: 1667. Find Kth Bit in Nth Binary String
+ Language: cpp
+ Runtime: 0 ms (100.00%)
+ Memory: 7.8 MB (63.07%)
+ Tags: String, Recursion, Simulation
+*/
+class Solution {
+public:
+    char findKthBit(int n, int k) {
+        if (n == 1) return '0';
+        
+        int mid = 1 << (n - 1);
+        
+        if (k == mid) return '1';
+        
+        if (k < mid)
+            return findKthBit(n - 1, k);
+        
+        char res = findKthBit(n - 1, (1 << n) - k);
+        return res == '0' ? '1' : '0';
+    }
+};
