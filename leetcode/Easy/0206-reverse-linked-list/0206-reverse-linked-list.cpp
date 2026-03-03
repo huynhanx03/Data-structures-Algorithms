18b1ce61c85ad483708d8c5082358ba348c552ff
huynhanx03 <157712338+huynhanx03@users.noreply.github.com>
2026-03-03 13:47:53 +0700

    Time: 0 ms (100.00%), Space: 13.4 MB (72.27%) - Reverse Linked List

---
+++ leetcode/Easy/0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -0,0 +1,31 @@
+/*
+ Problem: 206. Reverse Linked List
+ Language: cpp
+ Runtime: 0 ms (100.00%)
+ Memory: 13.4 MB (72.27%)
+ Tags: Linked List, Recursion
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            ListNode* nextTemp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+};
