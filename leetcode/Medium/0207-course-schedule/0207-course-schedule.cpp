cba13ed153b01c83126cc01e881fac72732421b8
huynhanx03 <157712338+huynhanx03@users.noreply.github.com>
2026-03-03 13:48:01 +0700

    Time: 14 ms (12.73%), Space: 19.7 MB (45.67%) - Course Schedule

---
+++ leetcode/Medium/0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,41 @@
+/*
+ Problem: 207. Course Schedule
+ Language: cpp
+ Runtime: 14 ms (12.73%)
+ Memory: 19.7 MB (45.67%)
+ Tags: Depth-First Search, Breadth-First Search, Graph Theory, Topological Sort
+*/
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        vector<int> adj[n];
+        vector<int> indegree(n, 0);
+        vector<int> ans;
+
+        for(auto x: prerequisites){
+            adj[x[0]].push_back(x[1]);
+            indegree[x[1]]++;
+        }
+
+        queue<int> q;
+        for(int i = 0; i < n; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+            auto t = q.front();
+            ans.push_back(t);
+            q.pop();
+
+            for(auto x: adj[t]){
+                indegree[x]--;
+                if(indegree[x] == 0){
+                    q.push(x);
+                }
+            }
+        }
+        return ans.size() == n;
+    }
+};
