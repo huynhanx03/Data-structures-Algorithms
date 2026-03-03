91d064a3932116ae20a980a3a2843763a9538083
huynhanx03 <157712338+huynhanx03@users.noreply.github.com>
2026-03-03 13:47:36 +0700

    Time: 0 ms (100.00%), Space: 8 MB (67.02%) - Add Two Integers

---
+++ leetcode/Easy/2383-add-two-integers/2383-add-two-integers.cpp
@@ -0,0 +1,13 @@
+/*
+ Problem: 2383. Add Two Integers
+ Language: cpp
+ Runtime: 0 ms (100.00%)
+ Memory: 8 MB (67.02%)
+ Tags: Math
+*/
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+};
