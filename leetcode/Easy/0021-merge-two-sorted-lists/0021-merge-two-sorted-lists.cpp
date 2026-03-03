8311671e6d71d42451741233981ddc95b50a2da3
huynhanx03 <157712338+huynhanx03@users.noreply.github.com>
2026-03-03 13:48:11 +0700

    Time: 15 ms (0.69%), Space: 14.8 MB (99.98%) - Merge Two Sorted Lists

---
+++ leetcode/Easy/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -0,0 +1,32 @@
+/*
+ Problem: 21. Merge Two Sorted Lists
+ Language: cpp
+ Runtime: 15 ms (0.69%)
+ Memory: 14.8 MB (99.98%)
+ Tags: Linked List, Recursion
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (list1 == NULL) return list2;
+        if (list2 == NULL) return list1;
+        
+        if (list1->val < list2->val) {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        } else {
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        }
+    }
+};
