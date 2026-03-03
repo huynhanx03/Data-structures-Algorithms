0a5a375390cb589e03ee24df615464067ac7e623
huynhanx03 <157712338+huynhanx03@users.noreply.github.com>
2026-03-03 13:47:40 +0700

    Time: 6 ms (60.80%), Space: 27.6 MB (86.31%) - Count Square Submatrices with All Ones

---
+++ leetcode/Medium/1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -0,0 +1,26 @@
+/*
+ Problem: 1402. Count Square Submatrices with All Ones
+ Language: cpp
+ Runtime: 6 ms (60.80%)
+ Memory: 27.6 MB (86.31%)
+ Tags: Array, Dynamic Programming, Matrix
+*/
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        int res = 0;
+
+        for (int i = 0; i < matrix.size(); ++i) {
+            for (int j = 0; j < matrix[0].size(); ++j) {
+                if (matrix[i][j] && i > 0 && j > 0) {
+                    matrix[i][j] = min({matrix[i - 1][j], 
+                        matrix[i][j - 1], matrix[i - 1][j - 1]}) + 1;
+                }
+
+                res += matrix[i][j];
+            }
+        }
+
+        return res;
+    }
+};
