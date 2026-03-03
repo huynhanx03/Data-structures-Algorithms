7c6a1cb16a2d5a36af551a302f12be7cb47594f2
huynhanx03 <157712338+huynhanx03@users.noreply.github.com>
2026-03-03 13:47:59 +0700

    Time: 0 ms (100.00%), Space: 14.9 MB (73.85%) - Partition List

---
+++ leetcode/Medium/0086-partition-list/0086-partition-list.cpp
@@ -0,0 +1,31 @@
+/*
+ Problem: 86. Partition List
+ Language: cpp
+ Runtime: 0 ms (100.00%)
+ Memory: 14.9 MB (73.85%)
+ Tags: Linked List, Two Pointers
+*/
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode before(0), after(0);
+        ListNode* before_curr = &before;
+        ListNode* after_curr = &after;
+        
+        while(head) {
+            if(head->val < x) {
+                before_curr->next = head;
+                before_curr = head;
+            } else {
+                after_curr->next = head;
+                after_curr = head;
+            }
+            head = head->next;
+        }
+        
+        after_curr->next = nullptr;
+        before_curr->next = after.next;
+        
+        return before.next;
+    }
+};
