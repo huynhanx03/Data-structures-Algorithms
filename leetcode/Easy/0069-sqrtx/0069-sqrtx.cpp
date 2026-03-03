72c7eeac0849a7abf7158eb1ce4d2b9675b0146f
huynhanx03 <157712338+huynhanx03@users.noreply.github.com>
2026-03-03 13:48:07 +0700

    Time: 0 ms (100.00%), Space: 8.3 MB (99.97%) - Sqrt(x)

---
+++ leetcode/Easy/0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,13 @@
+/*
+ Problem: 69. Sqrt(x)
+ Language: cpp
+ Runtime: 2 ms (20.62%)
+ Memory: 8.3 MB (99.32%)
+ Tags: Math, Binary Search
+*/
+class Solution {
+public:
+    int mySqrt(int x) {
+        return sqrt(x);
+    }
+};
