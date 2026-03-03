88a2760b7e3c75dc87a5ea11c94ae9f19e368c75
huynhanx03 <157712338+huynhanx03@users.noreply.github.com>
2026-03-03 13:48:06 +0700

    Time: 984 ms (5.09%), Space: 74.7 MB (77.36%) - Longest Consecutive Sequence

---
+++ leetcode/Medium/0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,28 @@
+/*
+ Problem: 128. Longest Consecutive Sequence
+ Language: cpp
+ Runtime: 984 ms (5.09%)
+ Memory: 74.7 MB (77.36%)
+ Tags: Array, Hash Table, Union-Find
+*/
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> numSet(nums.begin(), nums.end());
+        int longest = 0;
+
+        for (int num : nums) {
+            if (numSet.find(num - 1) == numSet.end()) {
+                int length = 1;
+
+                while (numSet.find(num + length) != numSet.end()) {
+                    length++;
+                }
+
+                longest = max(longest, length);
+            }
+        }
+
+        return longest;
+    }
+};
