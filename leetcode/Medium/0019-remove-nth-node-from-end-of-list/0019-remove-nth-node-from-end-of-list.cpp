71af104635bc1536c611986a252f20c5b99d61e2
huynhanx03 <157712338+huynhanx03@users.noreply.github.com>
2026-03-03 13:48:00 +0700

    Time: 0 ms (100.00%), Space: 15 MB (25.17%) - Remove Nth Node From End of List

---
+++ leetcode/Medium/0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,38 @@
+/*
+ Problem: 19. Remove Nth Node From End of List
+ Language: cpp
+ Runtime: 0 ms (100.00%)
+ Memory: 15 MB (25.17%)
+ Tags: Linked List, Two Pointers
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* first = &dummy;
+        ListNode* second = &dummy;
+
+        for (int i = 0; i <= n; ++i) {
+            first = first->next;
+        }
+
+        while (first != nullptr) {
+            first = first->next;
+            second = second->next;
+        }
+        
+        second->next = second->next->next;
+        return dummy.next;
+    }
+};
