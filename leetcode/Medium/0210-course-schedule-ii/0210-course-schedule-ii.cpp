4e88f0de7d5abfa679be8446efeed0b195e5cbc6
huynhanx03 <157712338+huynhanx03@users.noreply.github.com>
2026-04-05 13:52:37 +0000

    Time: 2 ms (76.53%), Space: 18.3 MB (70.91%) - Course Schedule II

--- leetcode/Medium/0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ leetcode/Medium/0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,48 +1,44 @@
 /*
  Problem: 210. Course Schedule II
  Language: cpp
- Runtime: 8 ms (23.96%)
- Memory: 17.9 MB (94.45%)
+ Runtime: 2 ms (76.53%)
+ Memory: 18.3 MB (70.91%)
  Tags: Depth-First Search, Breadth-First Search, Graph Theory, Topological Sort
 */
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> graph(numCourses);
-        vector<int> inDegree(numCourses, 0);
-        vector<int> order;
+        vector<vector<int>> g(numCourses);
+        vector<int> d(numCourses, 0);
+        vector<int> res;
 
-        // Build the graph and in-degree array
         for (const auto& pre : prerequisites) {
-            graph[pre[1]].push_back(pre[0]);
-            inDegree[pre[0]]++;
+            g[pre[1]].push_back(pre[0]);
+            ++d[pre[0]];
         }
 
-        // Find all nodes with zero in-degree
         queue<int> q;
         for (int i = 0; i < numCourses; ++i) {
-            if (inDegree[i] == 0) {
+            if (d[i] == 0) {
                 q.push(i);
             }
         }
 
-        // Process the nodes
         while (!q.empty()) {
-            int course = q.front();
+            int x = q.front();
             q.pop();
-            order.push_back(course);
+            res.push_back(x);
 
-            for (int neighbor : graph[course]) {
-                inDegree[neighbor]--;
-                if (inDegree[neighbor] == 0) {
-                    q.push(neighbor);
+            for (const auto& y : g[x]) {
+                --d[y];
+                if (d[y] == 0) {
+                    q.push(y);
                 }
             }
         }
 
-        // Check for cycles
-        if (order.size() == numCourses) {
-            return order;
+        if (res.size() == numCourses) {
+            return res;
         } else {
             return {};
         }
