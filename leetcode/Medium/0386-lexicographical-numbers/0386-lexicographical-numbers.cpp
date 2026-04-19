068c05c761794cc2d9335c23a11756e822032f35
huynhanx03 <157712338+huynhanx03@users.noreply.github.com>
2026-04-19 13:58:56 +0000

    Time: 2 ms (48.48%), Space: 13.9 MB (79.46%) - Lexicographical Numbers

---
+++ leetcode/Medium/0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -0,0 +1,29 @@
+/*
+ Problem: 386. Lexicographical Numbers
+ Language: cpp
+ Runtime: 2 ms (48.48%)
+ Memory: 13.9 MB (79.46%)
+ Tags: Depth-First Search, Trie
+*/
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> ans;
+        int cur = 1;
+
+        for (int i = 0; i < n; ++i) {
+            ans.push_back(cur);
+
+            if (cur * 10 <= n) {
+                cur *= 10;
+            } else {
+                while (cur % 10 == 9 || cur + 1 > n) {
+                    cur /= 10;
+                }
+                ++cur;
+            }
+        }
+        
+        return ans;
+    }
+};
