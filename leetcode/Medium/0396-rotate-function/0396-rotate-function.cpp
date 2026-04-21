ca7c352682ddbf10be597d3c1826c1e563a18a9d
huynhanx03 <157712338+huynhanx03@users.noreply.github.com>
2026-04-21 14:48:57 +0000

    Time: 0 ms (100.00%), Space: 99.6 MB (51.07%) - Rotate Function

---
+++ leetcode/Medium/0396-rotate-function/0396-rotate-function.cpp
@@ -0,0 +1,26 @@
+/*
+ Problem: 396. Rotate Function
+ Language: cpp
+ Runtime: 0 ms (100.00%)
+ Memory: 99.6 MB (51.07%)
+ Tags: Array, Math, Dynamic Programming
+*/
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+        int n = nums.size();
+        long long sum = 0, f = 0;
+        for (int i = 0; i < n; ++i) {
+            sum += nums[i];
+            f += 1LL * i * nums[i];
+        }
+
+        long long ans = f;
+        for (int k = 1; k < n; ++k) {
+            f = f + sum - 1LL * n * nums[n - k];
+            ans = max(ans, f);
+        }
+
+        return (int)ans;
+    }
+};
