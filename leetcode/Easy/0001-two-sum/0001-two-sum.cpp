aaf0b22d6cc9f4ebd88884632ebf7bbc48524db4
huynhanx03 <157712338+huynhanx03@users.noreply.github.com>
2026-04-23 15:11:00 +0000

    Time: 7 ms (40.75%), Space: 16.4 MB (5.20%) - Two Sum

--- leetcode/Easy/0001-two-sum/0001-two-sum.cpp
+++ leetcode/Easy/0001-two-sum/0001-two-sum.cpp
@@ -1,21 +1,29 @@
 /*
  Problem: 1. Two Sum
  Language: cpp
- Runtime: 1112 ms (5.24%)
- Memory: 10.1 MB (100.00%)
+ Runtime: 7 ms (40.75%)
+ Memory: 16.4 MB (5.20%)
  Tags: Array, Hash Table
 */
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector <int> ans;
+
+        map<int, int> pos;
+        
         for (int i = 0; i < nums.size(); ++i)
-            for (int j = i + 1; j < nums.size(); ++j) 
-                if (nums[i] + nums[j] == target) {
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    break;
-                }
+            pos[nums[i]] = i;
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            int cur = target - nums[i];
+            if (pos.count(cur)) {
+                if (i == pos[cur]) continue;
+                ans.push_back(i);
+                ans.push_back(pos[cur]);
+                break;
+            }
+        }
         return ans;
     }
 };
