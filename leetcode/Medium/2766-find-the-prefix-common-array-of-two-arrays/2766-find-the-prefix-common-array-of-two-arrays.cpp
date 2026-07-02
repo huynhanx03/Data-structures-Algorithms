a17a99c5f6222ba6041dc73868acc950d640436b
huynhanx03 <huynhnhandanang@gmail.com>
2026-07-02 22:20:41 +0700

    Time: 15 ms (24.05%), Space: 94.4 MB (6.65%) - Find the Prefix Common Array of Two Arrays

--- leetcode/Medium/2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.cpp
+++ leetcode/Medium/2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,31 +1,31 @@
 /*
  Problem: 2766. Find the Prefix Common Array of Two Arrays
  Language: cpp
- Runtime: 51 ms (5.09%)
- Memory: 94.2 MB (5.09%)
+ Runtime: 15 ms (24.05%)
+ Memory: 94.4 MB (6.65%)
  Tags: Array, Hash Table, Bit Manipulation
 */
 class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        set<int> commonElements;
-        vector<int> result(A.size(), 0);
-        unordered_map<int, int> countA, countB;
+        set<int> cnt;
+        vector<int> res(A.size(), 0);
+        unordered_map<int, int> cntA, cntB;
 
         for (int i = 0; i < A.size(); ++i) {
-            countA[A[i]]++;
-            countB[B[i]]++;
+            cntA[A[i]]++;
+            cntB[B[i]]++;
 
-            if (countA[A[i]] > 0 && countB[A[i]] > 0) {
-                commonElements.insert(A[i]);
+            if (cntA[A[i]] > 0 && cntB[A[i]] > 0) {
+                cnt.insert(A[i]);
             }
-            if (countA[B[i]] > 0 && countB[B[i]] > 0) {
-                commonElements.insert(B[i]);
+            if (cntA[B[i]] > 0 && cntB[B[i]] > 0) {
+                cnt.insert(B[i]);
             }
 
-            result[i] = commonElements.size();
+            res[i] = cnt.size();
         }
 
-        return result;
+        return res;
     }
 };
