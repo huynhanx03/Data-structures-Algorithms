65da8576d80f8f974075e81402937a65022e09d6
huynhanx03 <huynhnhandanang@gmail.com>
2026-07-02 22:20:43 +0700

    Time: 55 ms (15.85%), Space: 90 MB (57.50%) - Minimum Operations to Make a Uni-Value Grid

--- leetcode/Medium/2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
+++ leetcode/Medium/2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,34 +1,28 @@
 /*
  Problem: 2160. Minimum Operations to Make a Uni-Value Grid
  Language: cpp
- Runtime: 49 ms (35.00%)
- Memory: 90.2 MB (40.42%)
+ Runtime: 55 ms (15.85%)
+ Memory: 90 MB (57.50%)
  Tags: Array, Math, Sorting, Matrix
 */
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
-        // value vs k*x
         vector <int> arr;
 
         for (const auto& v : grid) {
-            for (const auto& x: v) arr.push_back(x);
+            for (const auto& x: v) arr.emplace_back(x);
         }
 
         sort(arr.begin(), arr.end());
 
         int n = arr.size();
-        int pos = n / 2;
-        int mid = arr[pos];
+        int mid = arr[n / 2];
         int res = 0;
 
         for (int i = 0; i < n; ++i) {
-            if (i == pos) continue;
-
             int diff = abs(arr[i] - mid);
-
             if (diff % x != 0) return -1;
-
             res += diff / x;
         }
 
